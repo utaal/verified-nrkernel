3f11bce4db7bc04b7847a8298cc3b92ab9b79e7c
Gerd Zellweger <mail@gerdzellweger.com>
2022-09-19 16:37:56 -0700

    Implement cr3 and alloc_page.
    Signed-off-by: Gerd Zellweger <mail@gerdzellweger.com>

--- page-table/mem_t.rs
+++ page-table/mem_t.rs
@@ -14,33 +14,30 @@ pub fn word_index(offset: usize) -> usize
 
 // FIXME: We need to allow the dirty and accessed bits to change in the memory.
 // Or maybe we just specify reads to return those bits as arbitrary?
-#[verifier(external_body)]
 pub struct PageTableMemory {
     /// `ptr` is the starting address of the physical memory linear mapping
-    ptr: *mut u64,
+    pub ptr: *mut u64,
+    pub pml4: usize,
+    pub pt_allocator: alloc::boxed::Box<dyn FnMut() -> usize>,
 }
 
 impl PageTableMemory {
     /// `cr3` returns the physical address at which the layer 0 page directory is mapped as well as
     /// the corresponding memory region
-    #[verifier(external_body)]
     pub fn cr3(&self) -> ((), usize)
     {
-        // FIXME:
-        unimplemented!()
+        ((), self.pml4)
     }
 
     // We assume that alloc_page never fails. In practice we can just keep a buffer of 3+ pages
     // that are allocated before we use map_frame.
     /// Allocates one page and returns its physical address
-    #[verifier(external_body)]
     pub fn alloc_page(&mut self) -> MemRegionExec
     {
-        // FIXME:
-        unimplemented!()
+        let base = (self.pt_allocator)();
+        MemRegionExec { base: base, size: 4096 }
     }
 
-    #[verifier(external_body)]
     pub fn write(&mut self, offset: usize, region: (), value: u64)
     {
         let word_offset: isize = word_index(offset) as isize;
@@ -49,7 +46,6 @@ impl PageTableMemory {
         }
     }
 
-    #[verifier(external_body)]
     pub fn read(&self, offset: usize, region: ()) -> u64
     {
         let word_offset: isize = word_index(offset) as isize;
