75f0dd51abff6bec01f6a678686b8fda5d856e6f
Reto Achermann <achreto@gmail.com>
2023-02-23 13:39:06 -0800

    nr: implement seq_to_set
    Signed-off-by: Reto Achermann <achreto@gmail.com>

--- nr/spec/utils.rs
+++ nr/spec/utils.rs
@@ -8,7 +8,7 @@ use super::pervasive::seq::*;
 #[allow(unused_imports)] // XXX: should not be needed!
 use super::pervasive::set::Set;
 #[allow(unused_imports)] // XXX: should not be needed!
-use super::pervasive::map::Map;
+use super::pervasive::map::*;
 #[allow(unused_imports)] // XXX: should not be needed!
 use super::pervasive::arbitrary;
 
@@ -28,54 +28,119 @@ pub open spec fn seq_disjoint<A>(s: Seq<A>, t: Seq<A>) -> bool
 }
 
 
-pub open spec fn seq_to_set<A>(seq: Seq<A>) -> Set<A>
-    decreases seq.len()
+/// recursive definition of seq to set conversion
+pub open spec fn seq_to_set_rec<A>(seq: Seq<A>) -> Set<A>
+    decreases seq.len() when seq.len() >= 0
 {
     if seq.len() == 0 {
         Set::empty()
     } else {
-        seq_to_set(seq.drop_last()).insert(seq.last())
+        seq_to_set_rec(seq.drop_last()).insert(seq.last())
     }
+}
+
+/// shows that the recursive definition of set_to_seq produces a finite set
+pub proof fn seq_to_set_rec_is_finite<A>(seq: Seq<A>)
+    ensures seq_to_set_rec(seq).finite()
+    decreases seq.len()
+{
+    if seq.len() > 0{
+        let sub_seq = seq.drop_last();
+        assert(seq_to_set_rec(sub_seq).finite()) by {
+            seq_to_set_rec_is_finite(sub_seq);
+        }
+    }
+}
+
+/// shows that the resulting set contains all elements of the sequence
+pub proof fn seq_to_set_rec_contains<A>(seq: Seq<A>)
+    ensures forall |a| #[trigger] seq.contains(a) <==> seq_to_set_rec(seq).contains(a)
+    decreases seq.len()
+{
+    if seq.len() > 0 {
+        assert(forall |a| #[trigger] seq.drop_last().contains(a) <==> seq_to_set_rec(seq.drop_last()).contains(a)) by {
+            seq_to_set_rec_contains(seq.drop_last());
+        }
+
+        assert(seq.ext_equal(seq.drop_last().push(seq.last())));
+        assert forall |a| #[trigger] seq.contains(a) <==> seq_to_set_rec(seq).contains(a) by {
+            if !seq.drop_last().contains(a) {
+                if a == seq.last() {
+                    assert(seq.contains(a));
+                    assert(seq_to_set_rec(seq).contains(a));
+                } else {
+                    assert(!seq_to_set_rec(seq).contains(a));
+                }
+            }
+        }
+    }
+}
+
+pub open spec fn seq_to_set<A>(seq: Seq<A>) -> Set<A>
+{
+    Set::new(|a: A| seq.contains(a))
+}
+
+pub proof fn seq_to_set_equal_rec<A>(seq: Seq<A>)
+    ensures seq_to_set(seq) == seq_to_set_rec(seq)
+{
+    assert(forall |n| #[trigger] seq.contains(n) <==> seq_to_set_rec(seq).contains(n)) by {
+        seq_to_set_rec_contains(seq);
+    }
+    assert(forall |n| #[trigger] seq.contains(n) <==> seq_to_set(seq).contains(n));
+    assert(seq_to_set(seq).ext_equal(seq_to_set_rec(seq)));
 }
 
 
 pub proof fn seq_to_set_is_finite<A>(seq: Seq<A>)
     ensures seq_to_set(seq).finite()
-    decreases seq.len()
 {
-    if seq.len() > 0 {
-        let sub_set = seq_to_set(seq.drop_last());
-        assert(sub_set.finite()) by {
-            seq_to_set_is_finite(seq.drop_last());
-        }
-        assert(seq_to_set(seq) == sub_set.insert(seq.last()));
+    assert(seq_to_set(seq).finite()) by {
+        seq_to_set_equal_rec(seq);
+        seq_to_set_rec_is_finite(seq);
     }
 }
 
-pub proof fn seq_to_set_contains<A>(seq: Seq<A>)
-    ensures forall |a| #[trigger] seq.contains(a) ==> seq_to_set(seq).contains(a)
-    decreases seq.len()
+
+
+
+
+pub open spec fn map_new_rec<V>(dom: nat, val: V) -> Map<nat, V>
+    decreases dom when dom >= 0
 {
-    if seq.len() > 0 {
-        let elm = seq.last();
-        let sub_req = seq.drop_last();
-        let sub_set = seq_to_set(sub_req);
-        assert(forall |a| #[trigger] sub_req.contains(a) ==> seq_to_set(sub_req).contains(a)) by {
-            seq_to_set_contains(sub_req);
+    if dom == 0 {
+        map![ dom => val]
+    } else {
+        map_new_rec((dom - 1) as nat, val).insert(dom, val)
+    }
+}
+
+pub proof fn map_new_rec_dom_finite<V>(dom: nat, val: V)
+   ensures
+      map_new_rec(dom, val).dom().finite(),
+      forall |n: nat| 0 <= n <= dom <==> map_new_rec(dom, val).dom().contains(n),
+      forall |n| #[trigger] map_new_rec(dom, val).dom().contains(n) ==> map_new_rec(dom, val)[n] == val
+   decreases dom
+{
+    if dom == 0 {
+
+    } else {
+        let sub_dom = (dom - 1) as nat;
+        let sub_map = map_new_rec(sub_dom as nat, val);
+        assert(sub_map.dom().finite()) by {
+            map_new_rec_dom_finite(sub_dom, val);
         }
 
-        assert(seq.ext_equal(sub_req.push(elm)));
+        assert(forall |n: nat| 0 <= n <= sub_dom <==> sub_map.dom().contains(n)) by {
+            map_new_rec_dom_finite(sub_dom, val);
+        }
 
-        assert forall |a| #[trigger] sub_req.push(elm).contains(a) implies seq_to_set(seq).contains(a) by {
-            if sub_req.contains(a) {
-                assert(seq_to_set(seq).contains(a));
-            }
-            if a == elm {
-                assert(seq_to_set(seq).contains(elm));
-            }
+        assert(forall |n: nat| #[trigger] sub_map.dom().contains(n) ==> sub_map[n] == val) by {
+            map_new_rec_dom_finite(sub_dom, val);
         }
     }
 }
+
 
 
 pub open spec fn map_contains_value<K, V>(map: Map<K, V>, val: V) -> bool
