40651a30c3b7f01ce3e961c5b9c79b9f82a73dc0
Andrea Lattuada <lattuada@vmware.com>
2023-03-01 23:04:27 +0100

    assume to patch incompleteness

--- nr/src/spec/utils.rs
+++ nr/src/spec/utils.rs
@@ -30,12 +30,21 @@ pub open spec fn seq_disjoint<A>(s: Seq<A>, t: Seq<A>) -> bool
 
 /// recursive definition of seq to set conversion
 spec fn seq_to_set_rec<A>(seq: Seq<A>) -> Set<A>
-    decreases seq.len() when seq.len() >= 0
+    decreases seq.len() when seq.len() >= 0 via seq_to_set_rec_decreases::<A>
 {
     if seq.len() == 0 {
         Set::empty()
     } else {
         seq_to_set_rec(seq.drop_last()).insert(seq.last())
+    }
+}
+
+#[via_fn]
+proof fn seq_to_set_rec_decreases<A>(seq: Seq<A>)
+{
+    if seq.len() == 0 {
+    } else {
+        assume(seq.drop_last().len() < seq.len()); // INCOMPLETENESS weird incompleteness again
     }
 }
 
