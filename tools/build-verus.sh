ecb07e602263d5159dd25a0da337628ed52f35d2
Reto Achermann <achreto@gmail.com>
2024-02-28 15:51:09 -0800

    setup github workflow
    Signed-off-by: Reto Achermann <achreto@gmail.com>

---
+++ tools/build-verus.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -eu
+
+REPOSITORY_ROOT=$(git rev-parse --show-toplevel)
+VERUS_ROOT="${REPOSITORY_ROOT}/verus"
+
+echo "Repository root: ${REPOSITORY_ROOT}"
+echo "Verus root:      ${VERUS_ROOT}"
+
+pushd ${VERUS_ROOT}/source > /dev/null
+
+# check if we need to get Z3
+if [ -f z3 ]; then
+    echo "Z3 already exists"
+else
+    echo "Downloading Z3..."
+    ./tools/get-z3.sh > /dev/null
+fi
+
+# activate & build vargo
+source ../tools/activate
+
+# build verus
+env VERUS_Z3_PATH="$PWD/z3" vargo build --release
+
+popd > /dev/null
+
+# make verus binary accessible
+echo "${VERUS_ROOT}/source/target-verus/release" >>"$GITHUB_PATH"
