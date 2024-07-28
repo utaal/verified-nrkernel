69038960756ab2dff5dc2f454caa74bd52783d78
Johanna Polzin <jpolzin@student.ethz.ch>
2024-07-28 15:30:59 +0200

    proof small lemma about values()

--- page-table/impl_u/os_refinement.rs
+++ page-table/impl_u/os_refinement.rs
@@ -120,13 +120,38 @@ proof fn lemma_effective_mappings_unaffected_if_thread_state_constant(
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Map lemmata
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+proof fn lemma_map_insert_value<A, B>(map: Map<A, B>, key: A, value: B)
+    requires
+    ensures
+        map.insert(key, value).values().contains(value),
+{
+    assert(map.insert(key, value).dom().contains(key));
+    assert(map.insert(key, value)[key] == value);
+}
+
 proof fn lemma_map_insert_values_equality<A, B>(map: Map<A, B>, key: A, value: B)
     requires
         map.dom().contains(key),
     ensures
         map.values().insert(value) === map.insert(key, value).values().insert(map.index(key)),
 {
-    admit();
+  //  
+    assert forall |values| #![auto] map.values().insert(value).contains(values) implies map.insert(key, value).values().insert(map.index(key)).contains(values) by {
+        
+        if (values == value) {
+            lemma_map_insert_value(map, key, value);
+        } else {
+            let k = choose | some_key | #[trigger] map.dom().contains(some_key) && (map[some_key] == values);
+            assert(map.insert(key, value).dom().contains(k));
+            if (k == key) {
+                assert(map.index(key) == values);
+            } else {
+                assert(map[k] === map.insert(key, value)[k]);
+            }
+        }
+    }
+    assert( map.values().insert(value) =~= map.insert(key, value).values().insert(map.index(key)));
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
