d3de67375e7853c07a45119f86e395fc5b8cc718
Reto Achermann <achreto@gmail.com>
2025-04-16 08:13:47 -0700

    fix version in the update-verus.sh script
    Signed-off-by: Reto Achermann <achreto@gmail.com>

--- tools/update-verus.sh
+++ tools/update-verus.sh
@@ -6,6 +6,9 @@ VERUS_REPO="git@github.com:verus-lang/verus.git"
 # where to clone the repository into
 VERUS_DIR=".verus"
 
+# the verus release to be used
+VERUS_RELEASE="release/0.2025.04.12.04a69f9"
+
 # check if verus repo exists, clone it if needed
 if [ ! -d .verus ]; then
     git clone $VERUS_REPO $VERUS_DIR
@@ -15,7 +18,10 @@ fi
 pushd $VERUS_DIR/source
 
 # update repository
-git pull
+git fetch
+
+# checkout the verus release we want
+git checkout $VERUS_RELEASE
 
 # update z3
 ./tools/get-z3.sh
